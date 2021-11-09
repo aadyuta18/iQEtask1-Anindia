7996aed716f5e2992bd58d8acdd6e974cb2a7cfb
anindia <gadyutawati@gmail.com>
2021-11-09 23:35:30 +0700

    Print Hello World

---
+++ src/user.java
@@ -0,0 +1,20 @@
+public class user {
+    String kd;
+    String nama;
+
+    public String getKd() {
+        return kd;
+    }
+
+    public void setKd(String kd) {
+        this.kd = kd;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
